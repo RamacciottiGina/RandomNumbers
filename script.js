8e805bd3681806b30bd682095b45cdb7d66dc131
RamacciottiGina <ramacciottigina@gmail.com>
2023-05-19 12:51:34 -0300

    Add window load

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const crearBoton = document.querySelector('.contenedor');
-
- for (let i=0; i<=50; i++) {
-    const boton = document.createElement('button');
-    boton.className = 'boton'
-    boton.textContent= i;
-   
-    if (i%2==0) {
-        boton.style.background = ('lightblue');
-    }
-        else {
-        boton.style.background = ('orange');    
+window.addEventListener('load', () => {
+    const crearBoton = document.querySelector('.contenedor');
+    for (let i=0; i<=50; i++) {
+        const boton = document.createElement('button');
+        boton.className = 'boton'
+        boton.textContent= i;
+    
+        if (i%2==0) {
+            boton.style.background = ('lightblue');
         }
+            else {
+            boton.style.background = ('orange');    
+            }
 
-        boton.addEventListener('click', ()=> {
-            const clickMessage= document.querySelector('#click-message');
-            const parity = i %2 === 0 ? 'par' : 'impar';
-            clickMessage.innerText = `Numero ${i} es ${parity}`;
-        })
+            boton.addEventListener('click', ()=> {
+                const clickMessage= document.querySelector('#click-message');
+                const parity = i %2 === 0 ? 'par' : 'impar';
+                clickMessage.innerText = `Numero ${i} es ${parity}`;
+            });
 
-    crearBoton.appendChild(boton);
+        crearBoton.appendChild(boton);
     }
-
+});
     const random =document.getElementById('random');
     random.addEventListener('click', ()=> {
         var randomNumber = Math.floor(Math.random() * 51);
